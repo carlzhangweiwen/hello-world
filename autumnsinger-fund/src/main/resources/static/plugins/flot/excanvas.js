dd0a823a377a241ab106eca403a6133fe78ae540
zhangweiwen <zhangweiwen@zhangweiwendeMacBook-Pro.local>
2018-01-10 15:37:58 +0800

    kafka client java test

--- autumnsinger-fund/src/main/resources/static/plugins/flot/excanvas.js
+++ autumnsinger-fund/src/main/resources/static/plugins/flot/excanvas.js
@@ -1,4 +1,4 @@
-// Copyright 2006 Google Inc.
+d// Copyright 2006 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
