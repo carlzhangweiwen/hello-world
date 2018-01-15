0a3829342d133cf291da493d451b357e94c496a8
zhangweiwen <zhangweiwen@zhangweiwendeMacBook-Pro.local>
2018-01-15 14:35:43 +0800

    两种kafka客户端

--- carlDemo/src/main/java/HelloKafka2.java
+++ carlDemo/src/main/java/HelloKafka2.java
@@ -1,7 +1,7 @@
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.ListTopicsResult;
-import org.apache.kafka.clients.admin.TopicListing;
+//import org.apache.kafka.clients.admin.AdminClient;
+//import org.apache.kafka.clients.admin.AdminClientConfig;
+//import org.apache.kafka.clients.admin.ListTopicsResult;
+//import org.apache.kafka.clients.admin.TopicListing;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,21 +15,21 @@ public class HelloKafka2 {
     public static void main(String[] args) throws Exception{
         String kafkaConnect = "localhost:9091,localhost:9092,localhost:9093";
         Properties config = new Properties();
-        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConnect);
-
-        AdminClient admin = AdminClient.create(config);
-
-        Map<String, String> configs = new HashMap<String,String>();
-        int partitions = 1;
-        short replication = 1;
-
-//        CreateTopicsResult result = admin.createTopics(asList(new NewTopic("mynewtopic", partitions, replication).configs(configs)));
-
-        ListTopicsResult result = admin.listTopics();
-        for (Map.Entry<String, TopicListing> entry : result.namesToListings().get().entrySet()) {
-
-            System.out.println("topic {} created:"+ entry.getValue().name());
-        }
+//        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConnect);
+//
+//        AdminClient admin = AdminClient.create(config);
+//
+//        Map<String, String> configs = new HashMap<String,String>();
+//        int partitions = 1;
+//        short replication = 1;
+//
+////        CreateTopicsResult result = admin.createTopics(asList(new NewTopic("mynewtopic", partitions, replication).configs(configs)));
+//
+//        ListTopicsResult result = admin.listTopics();
+//        for (Map.Entry<String, TopicListing> entry : result.namesToListings().get().entrySet()) {
+//
+//            System.out.println("topic {} created:"+ entry.getValue().name());
+//        }
     }
 
 
--- carlDemo/src/main/java/kafka/MyConsumer.java
+++ carlDemo/src/main/java/kafka/MyConsumer.java
@@ -21,6 +21,7 @@ public class MyConsumer extends Thread {
         this.groupid = groupid;
     }
     public void run() {
+        System.out.println("groupid:" + groupid);
         Properties props = new Properties();
 
         props.put("bootstrap.servers", "localhost:9091,localhost:9092,localhost:9093");
@@ -28,28 +29,49 @@ public class MyConsumer extends Thread {
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
+        props.put("partition.assignment.strategy","range");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 
-        consumer.subscribe(Arrays.asList(topic));
-        consumer.seekToBeginning(new ArrayList<TopicPartition>());
+        consumer.subscribe(topic);
+//        consumer.seekToBeginning(new ArrayList<TopicPartition>());
 
-        Map<String, List<PartitionInfo>> listTopics = consumer.listTopics();
-        Set<Map.Entry<String, List<PartitionInfo>>> entries = listTopics.entrySet();
+//        Map<String, List<PartitionInfo>> listTopics = consumer.listTopics();
+//        Set<Map.Entry<String, List<PartitionInfo>>> entries = listTopics.entrySet();
+//
+//        for (Map.Entry<String, List<PartitionInfo>> entry:
+//             entries) {
+//            System.out.println("topic:" + entry.getKey());
+//
+//        }
 
-        for (Map.Entry<String, List<PartitionInfo>> entry:
-             entries) {
-            System.out.println("topic:" + entry.getKey());
-
-        }
+        consumer.seek(new HashMap<TopicPartition, Long>());
 
         while(true) {
-            ConsumerRecords<String, String> records = consumer.poll(1000);
-            for(ConsumerRecord<String, String> record : records) {
-                System.out.println("fetched from partition " + record.partition() + ", offset: " + record.offset() + ", message: " + record.value());
+            Map<String, ConsumerRecords<String, String>> records = consumer.poll(1000);
+
+            if(records != null) {
+                Set<Map.Entry<String, ConsumerRecords<String, String>>> entries = records.entrySet();
+                for (Map.Entry<String, ConsumerRecords<String, String>> entry : entries) {
+                    String key = entry.getKey();
+                    ConsumerRecords<String, String> value = entry.getValue();
+                    System.out.println(key + ":" + value.topic() + ":" + value.records());
+                    for (ConsumerRecord<String, String> rec :
+                            value.records()) {
+                        try {
+                            System.out.println(key + ":" + value.topic() + ":" + rec.offset() + "," + rec.partition() + "," + rec.value());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
+//            ConsumerRecords<String, String> records = consumer.poll(1000);
+//            for(ConsumerRecord<String, String> record : records) {
+//                System.out.println("fetched from partition " + record.partition() + ", offset: " + record.offset() + ", message: " + record.value());
+//            }
             //按分区读取数据
 //              for (TopicPartition partition : records.partitions()) {
 //                  List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
